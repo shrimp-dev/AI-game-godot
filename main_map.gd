8694c486f5f49aa0afdf57df2a4125e577ee80d4
pattrickx <pattrickx@edu.unifor.br>
2023-11-27 20:18:26 -0300

    spawn multi cells in random positions

---
+++ main_map.gd
@@ -0,0 +1,26 @@
+extends Node2D
+var cell = preload("res://cell_body.tscn")
+var spawnLocs = Array()
+var minDist = 18
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in 100:
+		var new_cell = cell.instantiate()
+		new_cell.global_position = getNextSpawnLoc()
+		add_child(new_cell)
+		
+func getNextSpawnLoc():
+	while true:
+		var newLoc = Vector2(randi_range(20,get_viewport().get_visible_rect().size[0]-20),randi_range(20,get_viewport().get_visible_rect().size[1]-20))
+		var tooClose = false
+		for loc in spawnLocs:
+			if newLoc.distance_to(loc) < minDist:
+				tooClose = true;
+				break;
+		if !tooClose:
+			spawnLocs.append(newLoc)
+			return newLoc
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
